36153256d12f59327fe57204117b6fa6885b06e1
Sam Vendittelli <sam.vendittelli@hotmail.com>
2022-10-20 16:55:46 +0100

    format all files with prettier

--- src/board/LabelRow.tsx
+++ src/board/LabelRow.tsx
@@ -2,12 +2,12 @@ import { cellColumns } from "../Game";
 import Row from "./Row";
 
 const LabelRow = () => (
-    <Row
-        edgeClass={'label corner-label'}
-        edgeLabel={''}
-        centreClass={'label column-label'}
-        centreData={cellColumns.map(label => ({ value: label }))}
-    />
+  <Row
+    edgeClass={"label corner-label"}
+    edgeLabel={""}
+    centreClass={"label column-label"}
+    centreData={cellColumns.map((label) => ({ value: label }))}
+  />
 );
 
 export default LabelRow;
